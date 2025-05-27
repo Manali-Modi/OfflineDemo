2aa2648d979b9bd04178a348500cc54f072fae64
Tatva09 <tatva09@MACM09.local>
2025-05-27 15:01:05 +0530

    auth screens UI

--- src/navigation/NavigationStack.tsx
+++ src/navigation/NavigationStack.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import ProfileScreen from '../screens/ProfileScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProfileScreen from '../screens/profile-screen/ProfileScreen';
+import SignupScreen from '../screens/auth-screens/SignupScreen';
+import LoginScreen from '../screens/auth-screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function NavigationStack() {
   return (
-    <Stack.Navigator initialRouteName="profile">
+    <Stack.Navigator initialRouteName="login">
+      <Stack.Screen
+        name="signup"
+        component={SignupScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen
         name="profile"
         component={ProfileScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
